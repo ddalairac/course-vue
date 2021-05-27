8fa188e5664d550a4525ccf409db1b19a3c3bf97
diegodalairac <ddalairac@santadertecnologia.com.ar>
2021-05-27 18:06:43 -0300

    basics

---
+++ 00-gs-03-rebuilding-the-app-with-vue/app.js
@@ -0,0 +1,31 @@
+Vue.createApp({
+    // hacer un atributo que es igual a una function, se puede sintetizar en definiendo la funcion 
+    // data: function(){} 
+    // data() {}
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
